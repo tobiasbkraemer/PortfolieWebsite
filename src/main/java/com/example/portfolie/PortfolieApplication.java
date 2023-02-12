b8659b52faed0970761067d43a83da48a702dcee
tobiasbkreamer <tobias@kraemerne.dk>
2023-02-12 17:50:34 +0100

    Website start

---
+++ src/main/java/com/example/portfolie/PortfolieApplication.java
@@ -0,0 +1,13 @@
+package com.example.portfolie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolieApplication.class, args);
+    }
+
+}
